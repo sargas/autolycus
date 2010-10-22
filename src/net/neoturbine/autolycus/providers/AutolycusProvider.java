7b95127835749cc00e73106a72f4bbf2c93a351d
Joseph Jon Booker <joe@neoturbine.net>
2010-10-22 01:56:25 -0500

    Added caching to routes table, made query threadsafe

--- src/net/neoturbine/autolycus/providers/AutolycusProvider.java
+++ src/net/neoturbine/autolycus/providers/AutolycusProvider.java
@@ -4,7 +4,7 @@
 package net.neoturbine.autolycus.providers;
 
 import java.util.ArrayList;
-
+import java.util.Date;
 import net.neoturbine.autolycus.BusTimeAPI;
 import net.neoturbine.autolycus.data.Route;
 import android.content.ContentProvider;
@@ -16,6 +16,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.text.format.DateUtils;
 import android.util.Log;
 
 /**
@@ -28,7 +29,7 @@ public class AutolycusProvider extends ContentProvider {
 
 	private static final String DATABASE_NAME = "cache.db";
 	private static final int DATABASE_VERSION = 1;
-	
+
 	private static final UriMatcher uriMatcher;
 	private static final int URI_ROUTES = 1;
 	private static final int URI_SYSTEMS = 2;
@@ -37,7 +38,7 @@ public class AutolycusProvider extends ContentProvider {
 		uriMatcher.addURI(AUTHORITY, Routes.TABLE_NAME, URI_ROUTES);
 		uriMatcher.addURI(AUTHORITY, Systems.TABLE_NAME, URI_SYSTEMS);
 	}
-	
+
 	private static class DatabaseHelper extends SQLiteOpenHelper {
 		DatabaseHelper(Context context) {
 			super(context,DATABASE_NAME,null,DATABASE_VERSION);
@@ -50,11 +51,12 @@ public class AutolycusProvider extends ContentProvider {
 			str.append("CREATE TABLE ").append(Routes.TABLE_NAME)
 			.append(" ('").append(Routes._ID).append("' INTEGER PRIMARY KEY AUTOINCREMENT, '")
 			.append(Routes.System).append("' VARCHAR(255), '")
+			.append(Routes.Expiration).append("' INTEGER, '")
 			.append(Routes.RouteNumber).append("' VARCHAR(255) UNIQUE, '")
 			.append(Routes.RouteName).append("' VARCHAR(255) UNIQUE")
 			.append(");");
 			db.execSQL(str.toString());
-			
+
 			str = new StringBuilder();
 			str.append("CREATE TABLE ").append(Systems.TABLE_NAME)
 			.append(" (").append(Systems._ID).append(" INTEGER PRIMARY KEY AUTOINCREMENT, '")
@@ -62,12 +64,12 @@ public class AutolycusProvider extends ContentProvider {
 			.append(Systems.Abbrivation).append("' VARCHAR(255) UNIQUE")
 			.append(");");
 			db.execSQL(str.toString());
-			
+
 			ContentValues cta = new ContentValues();
 			cta.put(Systems.Name, "Chicago Transit Authority");
 			cta.put(Systems.Abbrivation, "CTA");
 			db.insert(Systems.TABLE_NAME, null, cta);
-			
+
 			ContentValues trip = new ContentValues();
 			trip.put(Systems.Name, "Ohio State University TRIP");
 			trip.put(Systems.Abbrivation, "OSU TRIP");
@@ -83,7 +85,7 @@ public class AutolycusProvider extends ContentProvider {
 		}
 	}
 	private DatabaseHelper dbHelper;
-	
+
 
 	/* (non-Javadoc)
 	 * @see android.content.ContentProvider#delete(android.net.Uri, java.lang.String, java.lang.String[])
@@ -132,8 +134,8 @@ public class AutolycusProvider extends ContentProvider {
 	@Override
 	public Cursor query(Uri uri, String[] projection, String selection,
 			String[] selectionArgs, String sortOrder) {
-		SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
-		
+		final SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
 		switch(uriMatcher.match(uri)) {
 		case URI_ROUTES:
 			qb.setTables(Routes.TABLE_NAME);
@@ -146,28 +148,48 @@ public class AutolycusProvider extends ContentProvider {
 		default:
 			throw new IllegalArgumentException("Unknown URI " + uri);
 		}
-		SQLiteDatabase db = dbHelper.getReadableDatabase();
-		Cursor c = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
+		final SQLiteDatabase db = dbHelper.getReadableDatabase();
+		final Cursor c = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
 		c.setNotificationUri(getContext().getContentResolver(), uri);
 		return c;
 	}
-	
+
 	private void fetchRoutes(String system) {
-		SQLiteDatabase db = dbHelper.getWritableDatabase();
+		final SQLiteDatabase db = dbHelper.getWritableDatabase();
 		try {
-			db.delete(Routes.TABLE_NAME,
-					Routes.System +"=?", new String[] {system});
-			ArrayList<Route> routes = BusTimeAPI.getRoutes(getContext(), system);
-			for(Route r : routes) {
-				ContentValues cv = new ContentValues();
-				cv.put(Routes.RouteName, r.getName());
-				cv.put(Routes.RouteNumber, r.getRt());
-				cv.put(Routes.System, r.getSystem());
-				db.insert(Routes.TABLE_NAME, null, cv);
+			final SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+			qb.setTables(Routes.TABLE_NAME);
+			final Cursor c = qb.query(db,null,
+					Routes.System+" = ? AND "+
+					Routes.Expiration+" < ?",
+					new String[] {system, new Long(getToday()).toString()}, null, null, null);
+			if(!c.moveToLast()) {
+				c.close();return;
+			} else c.close();
+			synchronized(this) {
+				db.delete(Routes.TABLE_NAME,
+						Routes.System +"=?", new String[] {system});
+				ArrayList<Route> routes = BusTimeAPI.getRoutes(getContext(), system);
+				for(Route r : routes) {
+					ContentValues cv = new ContentValues();
+					cv.put(Routes.RouteName, r.getName());
+					cv.put(Routes.RouteNumber, r.getRt());
+					cv.put(Routes.System, r.getSystem());
+					cv.put(Routes.Expiration, getTomorrow());
+					db.insert(Routes.TABLE_NAME, null, cv);
+				}
 			}
 		} catch (Exception e) {
 			Log.e(TAG,e.toString());
 		}
+	}
+
+	private long getTomorrow() {
+		return getToday() + DateUtils.DAY_IN_MILLIS;
+	}
+
+	private long getToday() {
+		return new Date().getTime();
 	}
 
 	/* (non-Javadoc)
--- src/net/neoturbine/autolycus/providers/Routes.java
+++ src/net/neoturbine/autolycus/providers/Routes.java
@@ -15,12 +15,14 @@ public class Routes implements BaseColumns {
 	
 	public static final Uri CONTENT_URI = Uri.parse(
 			"content://" + AutolycusProvider.AUTHORITY + "/routes");
-	
+
 	public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.autolycus.routes";
-	
+
 	static final String TABLE_NAME = "routes";
-	
+
 	public static final String RouteNumber = "rt";
 	public static final String RouteName = "rtnm";
 	public static final String System = "system";
+
+	public static final String Expiration = "expires";
 }
