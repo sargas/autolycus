7d309173b92887afe8a830b202299f581c36a262
Joseph Jon Booker <joe@neoturbine.net>
2010-12-04 13:21:45 -0600

    Add options menu to about screen
    Add copyright header also

--- src/net/neoturbine/autolycus/AboutScreen.java
+++ src/net/neoturbine/autolycus/AboutScreen.java
@@ -1,14 +1,33 @@
 /**
- * 
+ * This file is part of Autolycus.
+ * Copyright 2010 Joseph Jon Booker.
+ *
+ * Autolycus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * Autolycus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with Autolycus.  If not, see <http://www.gnu.org/licenses/>.
  */
 package net.neoturbine.autolycus;
 
+import net.neoturbine.autolycus.prefs.Prefs;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.text.util.Linkify;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 /**
@@ -36,6 +55,23 @@ public class AboutScreen extends Activity {
 			return;
 		}
 		appname.setText(getString(R.string.app_name) + " " + info.versionName);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.about, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.about_prefs:
+			startActivity(new Intent(this, Prefs.class));
+			return true;
+		}
+		return false;
 	}
 
 }
